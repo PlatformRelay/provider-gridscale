64a5f5c6c2f78dee4ba7a468f327f1e365ce9cdd
Konrad Ferdinand Heimel <konradferdinand.heimel@rewe-group.com>
2026-07-28 21:11:45 +0200

    :bug: fix(backuplist): fix deprecated API, goimports, regenerate managed/deepcopy files
    - Replace managed.WithExternalConnecter with managed.WithExternalConnector (SA1019)
    - Suppress GetEventRecorderFor deprecation with nolint; direct replacement breaks
      compilation until crossplane-runtime migrates NewAPIRecorder to new events API
    - Remove embedded field selector o.Options.Gate -> o.Gate (QF1008)
    - Fix goimports import grouping in cluster controller_test.go
    - Run angryjet: reorder BackupList methods before Clone (alphabetical), add
      BackupListList.GetItems() to both cluster and namespaced zz_generated.managedlist.go
    - Run controller-gen: move StorageBackupEntry deepcopy funcs after CloneStatus
      (alphabetical type ordering) in both cluster and namespaced zz_generated.deepcopy.go

--- apis/namespaced/storage/v1alpha1/zz_generated.managed.go
+++ apis/namespaced/storage/v1alpha1/zz_generated.managed.go
@@ -8,6 +8,46 @@ package v1alpha1
 
 import xpv1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
 
+// GetCondition of this BackupList.
+func (mg *BackupList) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetManagementPolicies of this BackupList.
+func (mg *BackupList) GetManagementPolicies() xpv1.ManagementPolicies {
+	return mg.Spec.ManagementPolicies
+}
+
+// GetProviderConfigReference of this BackupList.
+func (mg *BackupList) GetProviderConfigReference() *xpv1.ProviderConfigReference {
+	return mg.Spec.ProviderConfigReference
+}
+
+// GetWriteConnectionSecretToReference of this BackupList.
+func (mg *BackupList) GetWriteConnectionSecretToReference() *xpv1.LocalSecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this BackupList.
+func (mg *BackupList) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetManagementPolicies of this BackupList.
+func (mg *BackupList) SetManagementPolicies(r xpv1.ManagementPolicies) {
+	mg.Spec.ManagementPolicies = r
+}
+
+// SetProviderConfigReference of this BackupList.
+func (mg *BackupList) SetProviderConfigReference(r *xpv1.ProviderConfigReference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+// SetWriteConnectionSecretToReference of this BackupList.
+func (mg *BackupList) SetWriteConnectionSecretToReference(r *xpv1.LocalSecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this Clone.
 func (mg *Clone) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
 	return mg.Status.GetCondition(ct)
@@ -85,45 +125,5 @@ func (mg *StorageImport) SetProviderConfigReference(r *xpv1.ProviderConfigRefere
 
 // SetWriteConnectionSecretToReference of this StorageImport.
 func (mg *StorageImport) SetWriteConnectionSecretToReference(r *xpv1.LocalSecretReference) {
-	mg.Spec.WriteConnectionSecretToReference = r
-}
-
-// GetCondition of this BackupList.
-func (mg *BackupList) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
-	return mg.Status.GetCondition(ct)
-}
-
-// GetManagementPolicies of this BackupList.
-func (mg *BackupList) GetManagementPolicies() xpv1.ManagementPolicies {
-	return mg.Spec.ManagementPolicies
-}
-
-// GetProviderConfigReference of this BackupList.
-func (mg *BackupList) GetProviderConfigReference() *xpv1.ProviderConfigReference {
-	return mg.Spec.ProviderConfigReference
-}
-
-// GetWriteConnectionSecretToReference of this BackupList.
-func (mg *BackupList) GetWriteConnectionSecretToReference() *xpv1.LocalSecretReference {
-	return mg.Spec.WriteConnectionSecretToReference
-}
-
-// SetConditions of this BackupList.
-func (mg *BackupList) SetConditions(c ...xpv1.Condition) {
-	mg.Status.SetConditions(c...)
-}
-
-// SetManagementPolicies of this BackupList.
-func (mg *BackupList) SetManagementPolicies(r xpv1.ManagementPolicies) {
-	mg.Spec.ManagementPolicies = r
-}
-
-// SetProviderConfigReference of this BackupList.
-func (mg *BackupList) SetProviderConfigReference(r *xpv1.ProviderConfigReference) {
-	mg.Spec.ProviderConfigReference = r
-}
-
-// SetWriteConnectionSecretToReference of this BackupList.
-func (mg *BackupList) SetWriteConnectionSecretToReference(r *xpv1.LocalSecretReference) {
 	mg.Spec.WriteConnectionSecretToReference = r
 }
