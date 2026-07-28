80e2d4e5359e637284abaa1de739d85278817366
Konrad Ferdinand Heimel <konradferdinand.heimel@rewe-group.com>
2026-07-28 22:39:28 +0200

    :electric_plug: feat(setup): wire PublicNetwork controller into setup_custom E8-S04

--- internal/controller/namespaced/setup_custom.go
+++ internal/controller/namespaced/setup_custom.go
@@ -8,6 +8,7 @@ import (
 	tjcontroller "github.com/crossplane/upjet/v2/pkg/controller"
 	ctrl "sigs.k8s.io/controller-runtime"
 
+	publicnetwork "github.com/PlatformRelay/provider-gridscale/internal/controller/namespaced/gridscale/publicnetwork"
 	backuplist "github.com/PlatformRelay/provider-gridscale/internal/controller/namespaced/storage/backuplist"
 )
 
@@ -15,6 +16,7 @@ import (
 func SetupCustom(mgr ctrl.Manager, o tjcontroller.Options) error {
 	for _, setup := range []func(ctrl.Manager, tjcontroller.Options) error{
 		backuplist.Setup,
+		publicnetwork.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
@@ -27,6 +29,7 @@ func SetupCustom(mgr ctrl.Manager, o tjcontroller.Options) error {
 func SetupCustomGated(mgr ctrl.Manager, o tjcontroller.Options) error {
 	for _, setup := range []func(ctrl.Manager, tjcontroller.Options) error{
 		backuplist.SetupGated,
+		publicnetwork.SetupGated,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
